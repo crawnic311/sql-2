1391ce82197deb05c43a0e3cb20ba3be79c9837d
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-07 14:14:58 -0600

    Problem 3 & 4 Join

--- join.sql
+++ join.sql
@@ -8,4 +8,8 @@ SELECT i.invoice_date, c.first_name, c.last_name, i.total FROM invoice i
 JOIN customer c ON i.customer_id = c.customer_id
 
 
---Problem 3
+--Problem 3 & 4
+SELECT c.first_name, c.last_name, e.first_name, e.last_name FROM employee e
+JOIN customer c ON c.support_rep_id = e.employee_id
+
+--Problem 5
