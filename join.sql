97138cdea66c4dae2529580c6792a1db92c223a9
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-07 14:33:41 -0600

    Join Finished

--- join.sql
+++ join.sql
@@ -13,3 +13,29 @@ SELECT c.first_name, c.last_name, e.first_name, e.last_name FROM employee e
 JOIN customer c ON c.support_rep_id = e.employee_id
 
 --Problem 5
+SELECT a.title, art.artist_id FROM artist art
+JOIN album a ON a.artist_id = art.artist_id
+
+--Problem 6
+SELECT pt.track_id FROM playlist_track pt
+JOIN playlist p ON p.playlist_id = pt.playlist_id
+WHERE p.name = 'Music'
+
+--Problem 7
+SELECT t.name FROM track t
+JOIN playlist_track pt ON pt.track_id = t.track_id
+JOIN playlist p ON p.playlist_id = pt.playlist_id
+
+--Problem 8
+SELECT t.name, p.name FROM track t
+JOIN playlist_track pt ON pt.track_id = t.track_id
+JOIN playlist p ON p.playlist_id = pt.playlist_id
+
+--Problem 9
+SELECT t.name, a.title FROM track t
+JOIN album a ON a.album_id = t.album_id
+JOIN genre g ON g.genre_id = t.genre_id
+WHERE g.name = 'Alternative & Punk'
+
+
+
