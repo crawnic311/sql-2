b7515366ec7282d8bc45ad37c0d61a1b43a4a4bd
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-07 14:10:10 -0600

    Problem 2 Join

--- join.sql
+++ join.sql
@@ -4,3 +4,8 @@ JOIN invoice_line il ON il.invoice_id = i.invoice_id
 WHERE unit_price > .99
 
 --Problem 2
+SELECT i.invoice_date, c.first_name, c.last_name, i.total FROM invoice i
+JOIN customer c ON i.customer_id = c.customer_id
+
+
+--Problem 3
