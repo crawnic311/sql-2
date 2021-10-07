1a37749e3aaff9718f52d46b589908a9d5d249b5
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-07 15:38:52 -0600

    Delte Finished

---
+++ delete.sql
@@ -0,0 +1,24 @@
+--Problem 1
+DONE
+
+--Problem 2
+DELETE FROM animals
+WHERE type = 'lion'
+
+
+--Problem 3
+DELETE FROM animals
+WHERE name LIKE 'M%'
+
+
+--Problem 4
+DELETE FROM animals
+WHERE age < 9
+
+
+
+
+
+
+
+
